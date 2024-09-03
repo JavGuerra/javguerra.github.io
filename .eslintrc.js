44c2e2b4c5d1240ede5a6a90d5c8643c1f811218
JavGuerra <javguerra@badared.com>
2024-09-03 14:18:30 +0200

    upgrade

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    extends: [
+      "plugin:astro/recommended",
+    ],
+    overrides: [
+      {
+        // Define the configuration for `.astro` file.
+        files: ["*.astro"],
+        // Allows Astro components to be parsed.
+        parser: "astro-eslint-parser",
+        // Parse the script in `.astro` as TypeScript by adding the following configuration.
+        // It's the setting you need when using TypeScript.
+        parserOptions: {
+          parser: "@typescript-eslint/parser",
+          extraFileExtensions: [".astro"],
+        },
+        processor: "astro/client-side-ts", // <- Uses the "client-side-ts" processor.
+        rules: {
+          // override/add rules settings here, such as:
+          // "astro/no-set-html-directive": "error"
+        },
+      },
+    ],
+  }
