643f03b7169b7ba6d1b7e9a916b0ae556fb27775
JavGuerra <javguerra@badared.com>
2026-08-28 00:35:28 +0200

    Astro 7 mejoras

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,7 +4,7 @@ import { glob } from 'astro/loaders';
 
 const posts = defineCollection({
   loader: glob({
-    pattern: '**/[^_]*.{md,mdx}',
+    pattern: '**/[!_]*.{md,mdx}',
     base: './src/content/posts',
   }),
 
