344746c6a0d422d3f1ff1e810a9feaff1494d8af
JavGuerra <javguerra@badared.com>
2026-08-29 12:53:45 +0200

    Astro 7 mejoras

--- src/scripts/postsUtils.ts
+++ src/scripts/postsUtils.ts
@@ -1,4 +1,5 @@
 import { getCollection } from 'astro:content';
+import { getPostSlug } from '@/scripts/urlUtils';
 
 export async function getSortedPosts() {
   const allPosts = await getCollection('posts');
@@ -7,37 +8,42 @@ export async function getSortedPosts() {
     (a, b) => b.data.pubDate.getTime() - a.data.pubDate.getTime()
   );
 }
+export async function getSortedPostsData(
+    { includeFirstParagraph = true } = {}
+) {
+    const sortedPosts = await getSortedPosts();
 
-export async function getSortedPostsPrefixed() {
-  const sortedPosts = await getSortedPosts();
+    if (sortedPosts.length === 0) return [];
 
-  if (sortedPosts.length === 0) {
-    return [];
-  }
+    return sortedPosts.map((post, index) => {
+        const title = index === 0 && !post.data.title.startsWith('Nuevo: ')
+            ? `Nuevo: ${post.data.title}`
+            : post.data.title;
 
-  const firstPost = sortedPosts[0];
-
-  const title = firstPost.data.title;
-
-  const prefixedTitle = title.startsWith('Nuevo: ')
-    ? title
-    : `Nuevo: ${title}`;
-
-  const clonedPosts = [...sortedPosts];
-
-  clonedPosts[0] = {
-    ...firstPost,
-    data: {
-      ...firstPost.data,
-      title: prefixedTitle,
-    },
-  };
-
-  return clonedPosts;
+        return {
+            frontmatter: {
+                title,
+                description: post.data.description,
+                author: post.data.author,
+                pubDate: post.data.pubDate,
+                coverImage: post.data.coverImage,
+            },
+            href: getPostSlug(post),
+            ...(includeFirstParagraph && {
+                firstParagraph: post.body
+                    ? post.body.split('\n\n')[0]
+                        .replace(/\[([^\]]+)\]\([^\)]+\)/g, '$1')
+                        .replace(/\*\*(.+?)\*\*/g, '<strong>$1</strong>')
+                        .replace(/\*(.+?)\*/g, '<em>$1</em>')
+                        .replace(/_(.+?)_/g, '<em>$1</em>')
+                    : '',
+            }),
+        };
+    });
 }
 
 export async function getNewPost() {
-  const sortedPosts = await getSortedPostsPrefixed();
+  const sortedPosts = await getSortedPostsData();
 
   return sortedPosts[0] ?? null;
 }
